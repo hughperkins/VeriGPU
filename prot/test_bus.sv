cff75c6f162fb54d4d8b7018a3aad33822a5c012
Hugh Perkins <hughperkins@gmail.com>
2022-03-02 19:34:51 -0500

    add memory writing

--- prot/test_bus.sv
+++ prot/test_bus.sv
@@ -3,6 +3,7 @@
 module comp(
     input [7:0] read_data,
     output reg [7:0] write_data,
+    output reg [7:0] write_addr,
     output reg [7:0] read_addr,
     input clk,
     input rst,
@@ -15,6 +16,8 @@ module comp(
         end else begin
             out <= read_data;
             cnt <= cnt + 1;
+            write_addr <= cnt + 10;
+            write_data <= cnt + 5;
         end
     end
     assign read_addr = cnt;
@@ -23,53 +26,63 @@ endmodule
 module mem(
     output reg [7:0] read_data,
     input [7:0] write_data,
+    input [7:0] write_addr,
     input [7:0] read_addr,
     input clk,
-    input [7:0] startup_write_data,
-    input [7:0] startup_write_addr,
-    input startup_wen
+    input [7:0] dbg_write_data,
+    input [7:0] dbg_write_addr,
+    input [7:0] dbg_read_addr,
+    output reg [7:0] dbg_read_data,
+    input dbg_wen
 );
     reg [7:0] mem[256];
+    /*
     initial begin
         $display("hello");
-        $monitor("mem read_addr=%d read_data=%d clk=%d swen=%b", read_addr, read_data, clk, startup_wen);
+        $monitor("mem read_addr=%d read_data=%d clk=%d swen=%b", read_addr, read_data, clk, dbg_wen);
         #200
         $finish;
     end
+    */
     always @(posedge clk) begin
         // assign read_data = 8'd123;
         // read_data = 8'd123;
-        if (startup_wen) begin
-            mem[startup_write_addr] <= startup_write_data;
+        if (dbg_wen) begin
+            mem[dbg_write_addr] <= dbg_write_data;
         end
-        //else begin
-          //  read_data <= mem[read_addr];
-        //end
+        else begin
+            mem[write_addr] <= write_data;
+        end
     end
     assign read_data = mem[read_addr];
+    assign dbg_read_data = mem[dbg_read_addr];
 endmodule
 
 module test_comp();
     wire [7:0] read_data;
     wire [7:0] write_data;
+    wire [7:0] write_addr;
     reg clk;
-    reg startup_wen;
-    reg [7:0] startup_write_data;
-    reg [7:0] startup_write_addr;
+    reg dbg_wen;
+    reg [7:0] dbg_write_data;
+    reg [7:0] dbg_write_addr;
+    reg [7:0] dbg_read_data;
+    reg [7:0] dbg_read_addr;
     wire [7:0] out;
     wire [7:0] read_addr;
     reg rst;
 
     comp comp1(
         .read_data(read_data), .write_data(write_data), .clk(clk), .out(out), .rst(rst),
-        .read_addr(read_addr)
+        .read_addr(read_addr), .write_addr(write_addr)
     );
     mem mem1(
         .read_data(read_data), .write_data(write_data), .clk(clk),
-        .read_addr(read_addr),
-        .startup_write_data(startup_write_data),
-        .startup_write_addr(startup_write_addr),
-        .startup_wen(startup_wen)
+        .read_addr(read_addr), .write_addr(write_addr),
+        .dbg_write_data(dbg_write_data),
+        .dbg_write_addr(dbg_write_addr),
+        .dbg_read_addr(dbg_read_addr), .dbg_read_data(dbg_read_data),
+        .dbg_wen(dbg_wen)
     );
 
     initial begin
@@ -80,18 +93,22 @@ module test_comp();
     end
 
     initial begin
-        $monitor("t=%d out=%d, read_addr=%d read_data=%d", $time, out, read_addr, read_data);
+        $monitor("t=%d out=%d, read_addr=%d read_data=%d dbg_read_addr=%d dbg_read_data=%d", $time, out, read_addr, read_data, dbg_read_addr, dbg_read_data);
         rst = 1;
-        startup_wen = 1;
+        dbg_wen = 1;
         for(int i = 0; i < 8; i++) begin
             #10
-            startup_write_data = i + 3;
-            startup_write_addr = i;
+            dbg_write_data = i + 3;
+            dbg_write_addr = i;
         end
         #10
-        startup_wen = 0;
+        dbg_wen = 0;
         #10 rst = 0;
-        #100
+        #100 rst = 1;
+        for(int i = 0; i < 8; i++) begin
+            #10
+            dbg_read_addr = i + 10;
+        end
         $finish();
     end
 
