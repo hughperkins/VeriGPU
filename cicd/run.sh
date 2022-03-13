357493bbf51b6eba797b4280778e30890edfae07
Hugh Perkins <hughperkins@gmail.com>
2022-03-13 12:55:58 -0400

    fix run.sh

--- cicd/run.sh
+++ cicd/run.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 iverilog -g2012 src/proc.sv
-verilator -sv --cc src/proc.sv
+verilator -sv --cc src/op_const.sv src/proc.sv
 python -V
 pip freeze
 yosys -V
@@ -13,5 +13,5 @@ pytest -v .
 bash test/reg_test.sh
 bash test/run_sv_tests.sh
 
-python toy_proc/timing.py --in-verilog src/proc.sv | tee build/timing-proc.txt
+python toy_proc/timing.py --in-verilog src/op_const.sv src/proc.sv | tee build/timing-proc.txt
 python toy_proc/timing.py --in-verilog src/const.sv src/int_div_regfile.sv src/apu.sv --top-module apu | tee build/timing-apu.txt
