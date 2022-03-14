200d2e82b7bb4333d72a6b5cea09110edc557e60
Hugh Perkins <hughperkins@gmail.com>
2022-03-14 06:17:09 -0400

    add src/const.sv to tiing run cmd

--- cicd/run.sh
+++ cicd/run.sh
@@ -15,5 +15,5 @@ pytest -v .
 bash test/reg_test.sh
 bash test/run_sv_tests.sh
 
-python toy_proc/timing.py --in-verilog src/op_const.sv src/proc.sv | tee build/timing-proc.txt
+python toy_proc/timing.py --in-verilog src/op_const.sv src/const.sv src/proc.sv | tee build/timing-proc.txt
 python toy_proc/timing.py --in-verilog src/const.sv src/int_div_regfile.sv src/apu.sv --top-module apu | tee build/timing-apu.txt
