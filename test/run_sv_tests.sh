747cb827d39cc1a98d8739fbda979917696508df
Hugh Perkins <hughperkins@gmail.com>
2022-03-15 05:47:28 -0400

    run_sv_tests just calls other test scripts

--- test/run_sv_tests.sh
+++ test/run_sv_tests.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# run the various _test.sv files
-
 set -e
 set -x
 
 run_verilog() {
-    iverilog -g2012 "$@"
-    ./a.out | tee /tmp/out.txt
+    $1 | tee /tmp/out.txt
     set +x
     if grep ERROR /tmp/out.txt; then {
         echo "ERROR"
@@ -16,10 +13,7 @@ run_verilog() {
     set -x
 }
 
-run_verilog test/mem_delayed_test.sv src/mem_delayed.sv src/const.sv
-run_verilog test/int_div_regfile_test.sv src/const.sv src/int_div_regfile.sv
-
-# run_verilog test/apu_test.sv src/const.sv src/apu.sv src/int_div_regfile.sv
-# run_verilog test/apu_regfile_test.sv src/apu.sv src/reg_file.sv src/const.sv src/int_div_regfile.sv
+run_verilog test/int_div_regfile_test.sh
+run_verilog test/mem_delayed_test.sh
 
 echo "PASS"
