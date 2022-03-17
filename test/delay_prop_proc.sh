914709cecab3bf2fb6ec891ae598a530a7574351
Hugh Perkins <hughperkins@gmail.com>
2022-03-16 20:41:56 -0400

    formatting

--- test/delay_prop_proc.sh
+++ test/delay_prop_proc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # calculates delay propagation per cycle for proc.sv, and any child modules
 
-python toy_proc/timing.py --in-verilog src/const.sv src/op_const.sv src/int_div_regfile.sv src/reg_file.sv src/proc.sv --top-module proc
+python toy_proc/timing.py --in-verilog src/const.sv src/op_const.sv src/int_div_regfile.sv \
+    src/reg_file.sv src/proc.sv --top-module proc
