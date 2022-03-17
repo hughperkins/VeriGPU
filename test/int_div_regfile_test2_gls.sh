12f6dc39fe30a8317d0ccf718b8c46c1793320ea
Hugh Perkins <hughperkins@gmail.com>
2022-03-17 19:13:23 -0400

    make int test2 gls use 6.v

--- test/int_div_regfile_test2_gls.sh
+++ test/int_div_regfile_test2_gls.sh
@@ -6,5 +6,5 @@ set -ex
 set -o pipefail
 
 python toy_proc/run_yosys.py --in-verilog src/const.sv src/int_div_regfile.sv --top-module int_div_regfile >/dev/null
-iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/2.v src/const.sv test/int_div_regfile_test2.sv
+iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/const.sv test/int_div_regfile_test2.sv
 ./a.out
