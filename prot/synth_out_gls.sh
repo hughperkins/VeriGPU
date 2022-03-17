c5ac97118018aec0664f984f87d010edecdb1d5a
Hugh Perkins <hughperkins@gmail.com>
2022-03-16 20:15:40 -0400

    reformat output slightly in synth_out_gls.sh

--- prot/synth_out_gls.sh
+++ prot/synth_out_gls.sh
@@ -2,6 +2,8 @@
 # run gate-level simulation on synth_out.sv
 
 set -ex
+set -o pipefail
 
 python toy_proc/run_yosys.py --in-verilog prot/synth_out.sv --top-module synth_out >/dev/null
-iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v prot/synth_out_test.sv && ./a.out 
+iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v prot/synth_out_test.sv
+./a.out
