ced860960591c5d69490fdf8b68cf4d7e158932d
Hugh Perkins <hughperkins@gmail.com>
2022-03-17 22:00:16 -0400

    increas comp_driver

--- src/comp_driver.sv
+++ src/comp_driver.sv
@@ -94,7 +94,7 @@ module comp_driver(
         // while(~halt && $time < 4040) begin
         // while(~halt && $time - t_at_reset < 3940) begin
         // while(~halt && $time - t_at_reset < 6000) begin
-        while(~halt && $time - t_at_reset < 10000) begin
+        while(~halt && $time - t_at_reset < 20000) begin
         // while(~halt && $time - t_at_reset < 1200) begin
             #10;
         end
