6aff6eef2a66e64a17963e46c7b86d5ddb788d57
Hugh Perkins <hughperkins@gmail.com>
2022-03-19 18:05:30 -0400

    fix path to int gls test; and add set -ex

--- test/gls_tests.sh
+++ test/gls_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-bash int_div_regfile_test_gls.sh
+set -ex
+set -o pipefail
+
+bash test/int_div_regfile_test_gls.sh
 bash test/mem_delayed_test_gls.sh
 bash test/int_div_regfile_comp_gls.sh
 bash test/proc_gls.sh
