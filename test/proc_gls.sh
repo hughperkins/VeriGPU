ce7b209ae3f98144e90897f926628a817ffdbb5e
Hugh Perkins <hughperkins@gmail.com>
2022-03-19 17:40:56 -0400

    proc_gls.sh, add assert.sv etc

--- test/proc_gls.sh
+++ test/proc_gls.sh
@@ -15,15 +15,15 @@ if [[ x$1 != x ]]; then {
     progs=$1
 } fi
 
-python toy_proc/run_yosys.py --in-verilog src/op_const.sv src/const.sv src/int_div_regfile.sv src/proc.sv \
-    --top-module proc >/dev/null
+# python toy_proc/run_yosys.py --in-verilog src/assert_ignore.sv src/op_const.sv src/const.sv src/int_div_regfile.sv src/proc.sv \
+#     --top-module proc >/dev/null
 
 for prog in ${progs}; do {
     python toy_proc/assembler.py --in-asm examples/${prog}.asm --out-hex build/prog.hex
     cat src/comp_driver.sv | sed -e "s/{PROG}/prog/g" > build/comp_driver.sv
 
-    iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/const.sv src/mem_delayed.sv \
-        src/comp.sv build/comp_driver.sv
+    iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/assert.sv src/const.sv \
+        src/mem_delayed_small.sv src/mem_delayed.sv src/comp.sv build/comp_driver.sv
     ./a.out | tee build/out.txt
     if  ! cat build/out.txt | grep '^out[ \.]' > build/out_only.txt; then {
         echo "grep failed"
