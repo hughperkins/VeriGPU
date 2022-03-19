06f0265e654a7448c21e6c6e751becc05e19ad8d
Hugh Perkins <hughperkins@gmail.com>
2022-03-19 18:19:44 -0400

    remove monitor insertion in mem delayed gls

--- test/mem_delayed_test_gls.sh
+++ test/mem_delayed_test_gls.sh
@@ -16,13 +16,13 @@ python toy_proc/run_yosys.py --in-verilog src/mem_delayed_small.sv \
     src/assert_ignore.sv src/const.sv src/mem_delayed.sv \
     --top-module mem_delayed >/dev/null
 
-cat <<EOF > build/insert_monitor.txt
-  always @(*) begin
-    \$monitor("t=%0d 6.v.always* rst=%0d ena=%0d rd_req=%0d wr_req=%0d addr=%0d rd_data=%0d wr_data=%0d busy=%0d ack=%0d clks_to_wait=%0d",
-      \$time, rst, ena, rd_req, wr_req, addr, rd_data, wr_data, busy, ack, clks_to_wait);
-  end
-EOF
-sed -i -e '/_0000_/r build/insert_monitor.txt' build/netlist/6.v
+# cat <<EOF > build/insert_monitor.txt
+#   always @(*) begin
+#     \$monitor("t=%0d 6.v.always* rst=%0d ena=%0d rd_req=%0d wr_req=%0d addr=%0d rd_data=%0d wr_data=%0d busy=%0d ack=%0d clks_to_wait=%0d",
+#       \$time, rst, ena, rd_req, wr_req, addr, rd_data, wr_data, busy, ack, clks_to_wait);
+#   end
+# EOF
+# sed -i -e '/_0000_/r build/insert_monitor.txt' build/netlist/6.v
 
 iverilog -Wall -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/const.sv src/assert.sv \
     src/mem_delayed_small.sv test/mem_delayed_test.sv
