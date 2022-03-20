961ebc50b6927c7bc91ee85a1c4e5e3ba7d53b79
Hugh Perkins <hughperkins@gmail.com>
2022-03-20 05:39:33 -0400

    fix path in proc_gls.sh

--- test/gls/gls_tests.sh
+++ test/gls/gls_tests.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 bash test/gls/int_div_regfile_test_gls.sh
 bash test/gls/mem_delayed_test_gls.sh
-bash test/gls/int_div_regfile_comp_gls.sh
+bash test/gls/int_div_regfile_comp_test_gls.sh
 bash test/gls/proc_gls.sh
