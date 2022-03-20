b084437c52a70f9a6f18bf2d12858baf156b2775
Hugh Perkins <hughperkins@gmail.com>
2022-03-20 17:41:16 -0400

    fuse ops and shift

--- examples/test_ops.asm
+++ examples/test_ops.asm
@@ -26,4 +26,36 @@ li x3, 0b0110
 and x1, x2, x3
 outr x1  ; 0b0010
 
+# try add, sll, srl
+
+li x1 15
+li x2 8
+add x3, x1, x2
+outr x3
+
+li x2 1
+sll x3, x1, x2
+outr x3
+
+li x2 2
+sll x3, x1, x2
+outr x3
+
+li x2 3
+sll x3, x1, x2
+outr x3
+
+li x2 1
+srl x3, x1, x2
+outr x3
+
+li x2 2
+srl x3, x1, x2
+outr x3
+
+li x2 3
+srl x3, x1, x2
+outr x3
+
+
 halt
