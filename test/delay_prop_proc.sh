f41fee5480b56761060eff6f1ec38030b60a1cb2
Hugh Perkins <hughperkins@gmail.com>
2022-03-19 20:18:51 -0400

    fux delay prop proc (add assert_ignore

--- test/delay_prop_proc.sh
+++ test/delay_prop_proc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # calculates delay propagation per cycle for proc.sv, and any child modules
 
-python toy_proc/timing.py --in-verilog src/const.sv src/op_const.sv src/int_div_regfile.sv \
+python toy_proc/timing.py --in-verilog src/const.sv src/op_const.sv \
+    src/assert_ignore.sv src/int_div_regfile.sv \
     src/reg_file.sv src/proc.sv --top-module proc
