001bb11ae3c6f075ddee975cd7cfb7d21508b272
Hugh Perkins <hughperkins@gmail.com>
2022-03-20 17:39:23 -0400

    merge test_addi

--- examples/test_addi.asm
+++ examples/test_addi.asm
@@ -12,4 +12,30 @@ outr x5
 outr x3
 outr x5
 
+# negative immediate for addi
+
+li x1, 123
+outr x1
+
+addi x3, x1, 2
+outr x3
+
+addi x3, x1, -1
+outr x3
+
+addi x3, x1, -10
+outr x3
+
+addi x3, x1, -100
+outr x3
+
+addi x3, x0, -1000
+outr x3
+
+addi x3, x0, -2000
+outr x3
+
+addi x3, x0, 2000
+outr x3
+
 halt
