502f1761404fb16f84e5730ba6b544d68be3f73c
Hugh Perkins <hughperkins@gmail.com>
2022-03-22 04:47:16 -0400

    fix verilator compile check, and move to separate script

--- cicd/run-behav.sh
+++ cicd/run-behav.sh
@@ -4,9 +4,7 @@ set -x
 set -e
 set -o pipefail
 
-# iverilog -g2012 src/op_const.sv src/const.sv src/int_div_regfile.sv src/proc.sv
-# check that verilator compilation succeeds (verilator acts as a type of linter here)
-verilator -sv --cc src/op_const.sv src/assert.sv src/const.sv src/int_div_regfile.sv src/proc.sv
+test/behav/verilator_compile_proc.sh
 
 python -V
 yosys -V
