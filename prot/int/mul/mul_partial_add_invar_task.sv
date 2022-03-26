3466981bed45718b6ef12e83f40aad1583ced9e5
Hugh Perkins <hughperkins@gmail.com>
2022-03-26 07:04:53 -0400

    reduce latency for parital_add_invar by half...

--- prot/int/mul/mul_partial_add_invar_task.sv
+++ prot/int/mul/mul_partial_add_invar_task.sv
@@ -1,13 +1,18 @@
-// handles adding a single bit of partial products, and returning the sum
-// takes as input the carry from previous additional
-// thus we do one bit per cycle, and this task represents a single cycle
-//
-// in this version, we try to keep the loop invariant of pos, by shifting
-// operands beforehand, and padding them.
-// This is mostly in preparation for trying to use full adders to maximum
-// efficiency, similar to Dadda
+/*
+handles adding a single bit of partial products, and returning the sum
+takes as input the carry from previous additional
+thus we do one bit per cycle, and this task represents a single cycle
 
-task mul_partial_add_task(
+in this version, we try to keep the loop invariant of pos, by shifting
+operands beforehand, and padding them.
+This is mostly in preparation for trying to use full adders to maximum
+efficiency, similar to Dadda
+
+Using width 32, bits per cycle 1:
+Max propagation delay: 69.8 nand units
+Area:                  907.5 nand units
+*/
+task mul_partial_add_invar_task(
     input [$clog2(width + 1):0] pos,
     input [width - 1:0] a,
     input [width - 1:0] b,
@@ -26,8 +31,6 @@ task mul_partial_add_task(
     a_shifted = '0;
     a_shifted = a << (width - pos);
     for(int i = 0; i < width; i++) begin  // iterate through b
-        if(b[i]) begin
-            {cout, sum} = {cout, sum} + a_shifted[width - i];
-        end
+        {cout, sum} = {cout, sum} + (a_shifted[width - i] & b[i]);
     end
 endtask
