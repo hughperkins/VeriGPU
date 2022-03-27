8ef7ac7de7e70504ff9fb4f6a1b16ff44f54aecd
Hugh Perkins <hughperkins@gmail.com>
2022-03-26 20:25:49 -0400

    fix another test

--- test/behav/mem_delayed_test.sh
+++ test/behav/mem_delayed_test.sh
@@ -3,5 +3,5 @@
 set -ex
 set -o pipefail
 
-iverilog -Wall -g2012 src/assert.sv src/mem_delayed_small.sv src/const.sv src/mem_delayed.sv test/mem_delayed_test.sv
+iverilog -Wall -g2012 src/assert.sv src/mem_delayed_small.sv src/const.sv src/mem_delayed.sv test/behav/mem_delayed_test.sv
 ./a.out
