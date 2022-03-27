99bc29976dd326c110a87a6ce6157e3216cd1a8a
Hugh Perkins <hughperkins@gmail.com>
2022-03-27 19:52:54 -0400

    maybe fix another test

--- test/gls/proc_gls.sh
+++ test/gls/proc_gls.sh
@@ -24,6 +24,7 @@ if [[ -f build/netlist/6.v ]]; then {
 
 python verigpu/run_yosys.py --in-verilog src/assert_ignore.sv src/op_const.sv src/const.sv \
     src/float/float_params.sv src/float/float_add_pipeline.sv \
+    src/generated/mul_pipeline_cycle_24bit_2bpc.sv src/float/float_mul_pipeline.sv \
     src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
     src/int/int_div_regfile.sv src/proc.sv \
     --top-module proc >/dev/null
