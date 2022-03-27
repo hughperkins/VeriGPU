459e2d9c12de6bcf36c8a605881aa84877b268d0
Hugh Perkins <hughperkins@gmail.com>
2022-03-26 20:18:56 -0400

    more fixes after move

--- test/gls/int_div_regfile_test_gls.sh
+++ test/gls/int_div_regfile_test_gls.sh
@@ -5,6 +5,6 @@
 set -ex
 set -o pipefail
 
-python toy_proc/run_yosys.py --in-verilog src/assert_ignore.sv src/const.sv src/int_div_regfile.sv --top-module int_div_regfile >/dev/null
+python toy_proc/run_yosys.py --in-verilog src/assert_ignore.sv src/const.sv src/int/int_div_regfile.sv --top-module int_div_regfile >/dev/null
 iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/assert.sv src/const.sv test/behav/int_div_regfile_test.sv
 ./a.out
