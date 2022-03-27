4d7d9cbedf4d92c29246e66ce60213f655b11363
Hugh Perkins <hughperkins@gmail.com>
2022-03-27 18:57:46 -0400

    fix another tes

--- test/timing/delay_prop_proc.sh
+++ test/timing/delay_prop_proc.sh
@@ -6,5 +6,6 @@ set -o pipefail
 
 python verigpu/timing.py --in-verilog src/assert_ignore.sv src/const.sv src/op_const.sv \
     src/float/float_params.sv src/float/float_add_pipeline.sv \
+    src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
     src/assert_ignore.sv src/int/int_div_regfile.sv \
     src/proc.sv --top-module proc
