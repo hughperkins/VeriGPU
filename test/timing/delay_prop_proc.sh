0fe30cda33c1a03acfe8296b1c54f1b7253e7e9a
Hugh Perkins <hughperkins@gmail.com>
2022-03-26 20:36:43 -0400

    fixx another test hopefully

--- test/timing/delay_prop_proc.sh
+++ test/timing/delay_prop_proc.sh
@@ -4,6 +4,7 @@
 set -ex
 set -o pipefail
 
-python toy_proc/timing.py --in-verilog src/const.sv src/op_const.sv \
+python toy_proc/timing.py --in-verilog src/assert_ignore.sv src/const.sv src/op_const.sv \
+    src/float/float_params.sv src/float/float_add_pipeline.sv \
     src/assert_ignore.sv src/int/int_div_regfile.sv \
     src/reg_file.sv src/proc.sv --top-module proc
