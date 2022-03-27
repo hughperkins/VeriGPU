f71382da0c364b631524f120afb9ce101029ef5c
Hugh Perkins <hughperkins@gmail.com>
2022-03-27 19:50:38 -0400

    possibly fix more tests

--- test/timing/delay_prop_proc.sh
+++ test/timing/delay_prop_proc.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 python verigpu/timing.py --in-verilog src/assert_ignore.sv src/const.sv src/op_const.sv \
     src/float/float_params.sv src/float/float_add_pipeline.sv \
+    src/generated/mul_pipeline_cycle_24bit_2bpc.sv src/float/float_mul_pipeline.sv \
     src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
     src/assert_ignore.sv src/int/int_div_regfile.sv \
     src/proc.sv --top-module proc
