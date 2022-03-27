61cf297210e60f110016faaac7982222458b14f3
Hugh Perkins <hughperkins@gmail.com>
2022-03-27 19:01:00 -0400

    maybe fix more tests

--- test/gls/proc_gls.sh
+++ test/gls/proc_gls.sh
@@ -24,6 +24,7 @@ if [[ -f build/netlist/6.v ]]; then {
 
 python verigpu/run_yosys.py --in-verilog src/assert_ignore.sv src/op_const.sv src/const.sv \
     src/float/float_params.sv src/float/float_add_pipeline.sv \
+    src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
     src/int/int_div_regfile.sv src/proc.sv \
     --top-module proc >/dev/null
 
@@ -33,6 +34,7 @@ for prog in ${progs}; do {
 
     iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/assert_ignore.sv src/const.sv \
         src/float/float_params.sv src/float/float_add_pipeline.sv \
+        src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
         src/mem_delayed_large.sv src/mem_delayed.sv src/comp.sv build/comp_driver.sv
     ./a.out | tee build/out.txt
     if  ! cat build/out.txt | grep '^out[ \.]' > build/out_only.txt; then {
--- test/timing/delay_prop_whole_comp.sh
+++ test/timing/delay_prop_whole_comp.sh
@@ -10,6 +10,7 @@ set -o pipefail
 python verigpu/timing.py --in-verilog src/const.sv src/op_const.sv \
     src/assert_ignore.sv \
     src/float/float_params.sv src/float/float_add_pipeline.sv \
+    src/generated/mul_pipeline_cycle_32bit_2bpc.sv src/int/mul_pipeline_32bit.sv \
     src/int/int_div_regfile.sv \
     src/proc.sv src/mem_delayed_small.sv src/mem_delayed.sv src/comp.sv \
     --top-module comp
