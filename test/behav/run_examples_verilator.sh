df61c13c0dfedea3aadb12f2ac46b7d7ee6cafc8
Hugh Perkins <hughperkins@gmail.com>
2022-04-02 16:39:45 -0400

    can run examples using verilator now

---
+++ test/behav/run_examples_verilator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run all the examples, in examples folder
+# each example comprises an assembler program, that we can compile, and feed to a simulator
+# running our processor/gpu verilog
+
+set -e
+set -x
+set -o pipefail
+
+files=$(ls -b examples/*.asm)
+
+for prog in ${files}; do {
+    python run.py --verilator --name ${prog}
+} done
