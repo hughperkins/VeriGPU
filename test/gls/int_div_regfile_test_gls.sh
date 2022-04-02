4d6c1b3769b92eb01492fc15d5f764778c43d49c
Hugh Perkins <hughperkins@gmail.com>
2022-04-02 06:54:15 -0400

    maybe fix failing test

--- test/gls/int_div_regfile_test_gls.sh
+++ test/gls/int_div_regfile_test_gls.sh
@@ -5,6 +5,8 @@
 set -ex
 set -o pipefail
 
-python verigpu/run_yosys.py --in-verilog src/assert_ignore.sv src/const.sv src/int/int_div_regfile.sv --top-module int_div_regfile >/dev/null
-iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/assert.sv src/const.sv test/behav/int_div_regfile_test.sv
+python verigpu/run_yosys.py --in-verilog src/assert_ignore.sv src/const.sv src/int/int_div_regfile.sv \
+    --top-module int_div_regfile >/dev/null
+iverilog -g2012 tech/osu018/osu018_stdcells.v build/netlist/6.v src/assert.sv src/const.sv \
+    test/behav/int/int_div_regfile_test.sv
 ./a.out
