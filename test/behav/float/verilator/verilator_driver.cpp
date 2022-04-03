ccc50aa402e534f05d0d1a74c8be876caf2fb73f
Hugh Perkins <hughperkins@gmail.com>
2022-04-03 10:30:03 -0400

    remove dead code from verilator_driver

--- test/behav/float/verilator/verilator_driver.cpp
+++ test/behav/float/verilator/verilator_driver.cpp
@@ -26,11 +26,6 @@ int main(int argc, char **argv, char **env)
 
     Verilated::assertOn(false);
 
-    // Verilated::traceEverOn(true);
-    // VerilatedVcdC *m_trace = new VerilatedVcdC;
-    // dut->trace(m_trace, 5);
-    // m_trace->open("waveform.vcd");
-
     dut->rst = 1;
     dut->clk = 0;
     dut->eval();
@@ -48,12 +43,8 @@ int main(int argc, char **argv, char **env)
         dut->clk = 0;
         sim_time += 5;
         dut->eval();
-        dut->eval();
-        dut->eval();
         dut->clk = 1;
         sim_time += 5;
-        dut->eval();
-        dut->eval();
         dut->eval();
         unsigned int out = (unsigned int)(dut->out);
         if(int(dut->ack)) {
@@ -67,7 +58,6 @@ int main(int argc, char **argv, char **env)
     assert(~int(dut->fail));
     std::cout << "finished" << std::endl;
 
-    // m_trace->close();
     delete dut;
     exit(EXIT_SUCCESS);
 }
