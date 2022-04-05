2ed76f95959c9df7a80be3bb6232e5df7a5196b1
Hugh Perkins <hughperkins@gmail.com>
2022-04-05 06:18:48 -0400

    tentatively add check markdown to ci

--- cicd/check_markdown.sh
+++ cicd/check_markdown.sh
@@ -4,4 +4,7 @@
 # npm i --global -D markdown-link-check
 # page: https://github.com/tcort/markdown-link-check
 
+set -ex
+set -o pipefail
+
 find . -name \*.md -print0 | xargs -0 -n1 markdown-link-check -q -c cicd/markdown-link-check-config.json 
