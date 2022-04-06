bcbe165a9d2928d23c8f551c7d5c11ccaab5ec22
Hugh Perkins <hughperkins@gmail.com>
2022-04-05 20:45:51 -0400

    add single source to cicd

---
+++ cicd/run-single-source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ex
+set -o pipefail
+
+examples/cpp_single_source/run_all.sh
---
+++ examples/cpp_single_source/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ex
+set -o pipefail
+
+examples/cpp_single_source/data_transfer/run.sh
