7ef13cbadea092426df66a44a687a34abb6a678b
Hugh Perkins <hughperkins@gmail.com>
2022-04-11 06:18:38 -0400

    todo, docker, single source runall

--- examples/cpp_single_source/run_all.sh
+++ examples/cpp_single_source/run_all.sh
@@ -3,4 +3,8 @@
 set -ex
 set -o pipefail
 
+# first build the runtime and gpu...
+prot/verilator/prot_single_source/build-cmake.sh
+
 examples/cpp_single_source/data_transfer/run.sh
+examples/cpp_single_source/sum_ints/run.sh
