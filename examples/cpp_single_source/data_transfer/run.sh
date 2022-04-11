7bdb1a2003a1bc00976f8b41a00c957d35b525f3
Hugh Perkins <hughperkins@gmail.com>
2022-04-11 06:07:30 -0400

    fix data_transfer test

--- examples/cpp_single_source/data_transfer/run.sh
+++ examples/cpp_single_source/data_transfer/run.sh
@@ -11,22 +11,37 @@ set -o pipefail
 
 SCRIPTDIR=$(dirname $0)
 BASENAME=$(basename $SCRIPTDIR)
-
-VERILATORDIR=/usr/local/share/verilator
-# CLANGDIR=$HOME/Downloads/clang+llvm-14.0.0-x86_64-apple-darwin
-CLANGDIR=/usr/local/opt/llvm-14.0.0
-MACCLTINCLUDEDIR=/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/usr/include
-
 BASEDIR=$PWD
 SRC=${BASEDIR}/src
-
 export VERIGPUDIR=${BASEDIR}
+
+VERILATORDIR=/usr/local/share/verilator
+
+if [[ $(uname) == Linux ]]; then {
+    # assume installed clang 13 using https://apt.llvm.org/
+    # if you want to handle other scenarios, please submit a PR :)
+    echo Linux detected
+    CLANGPP=clang++-13
+    CLANG=clang-13
+    LLC=llc-13
+    GPURUNTIMEDIR=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-linux
+    LIBEXPFS=-lstdc++fs
+} elif [[ $(uname) == Darwin ]]; then {
+    echo Mac detected
+    CLANGDIR=/usr/local/opt/llvm-14.0.0
+    CLANGPP=${CLANGDIR}/bin/clang++
+    CLANG=${CLANGDIR}/bin/clang
+    LLC=${CLANGDIR}/bin/llc
+    BUILDDIR=cmake-mac
+    GPURUNTIMEDIR=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-mac
+    MACCLTINCLUDE="-I/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/usr/include"
+} fi
 
 cd ${SCRIPTDIR}
 
-# if [[ -d build_bash ]]; then {
-    # rm -R build_bash
-# } fi
+if [[ -d build_bash ]]; then {
+    rm -R build_bash
+} fi
 
 if [[ ! -d build_bash ]]; then {
     mkdir build_bash
@@ -34,70 +49,48 @@ if [[ ! -d build_bash ]]; then {
 
 cd build_bash
 
-# LIBRARY_PATH=${CLANGDIR}/
-
 # building patch_hostside is being migrated into cmake script at 
 # /prot/verilator/prot_single_source/CMakeLists.txt
 
 # host-side: -.cu => -hostraw.cll
-${CLANGDIR}/bin/clang++ \
+${CLANGPP} -fPIE \
     -std=c++11 -x cuda -nocudainc --cuda-host-only -emit-llvm \
     -I${CLANGDIR}/include \
     -I${CLANGDIR}/include/c++/v1 \
-    -I${MACCLTINCLUDEDIR} \
+    ${MACCLTINCLUDE} \
     -I${BASEDIR}/prot/verilator/prot_single_source \
     -S ../data_transfer.cpp \
     -o data_transfer-hostraw.ll
 
 # device-side => sum_ints.ll
-${CLANGDIR}/bin/clang++ \
+${CLANGPP} -fPIE \
     -x cuda \
     --cuda-device-only -emit-llvm \
     -nocudainc \
     -nocudalib \
     -I${CLANGDIR}/include \
     -I${CLANGDIR}/include/c++/v1 \
-    -I${MACCLTINCLUDEDIR} \
+    ${MACCLTINCLUDE} \
     -I${BASEDIR}/prot/verilator/prot_single_source \
     -S ../data_transfer.cpp \
     -o data_transfer-device.ll
 
-${CLANGDIR}/bin/llc data_transfer-device.ll -o data_transfer-device.s --march=riscv32
+${LLC} data_transfer-device.ll -o data_transfer-device.s --march=riscv32
 
 # now we have to patch hostside...
-${BASEDIR}/prot/verilator/prot_single_source/build-cmake-mac/patch_hostside \
+${GPURUNTIMEDIR}/patch_hostside \
      --devicellfile data_transfer-device.ll \
      --deviceriscvfile data_transfer-device.s \
      --hostrawfile data_transfer-hostraw.ll \
      --hostpatchedfile data_transfer-hostpatched.ll
 echo patched hostside
 
-${CLANGDIR}/bin/llc data_transfer-hostpatched.ll -o data_transfer-hostpatched.s
+${LLC} data_transfer-hostpatched.ll --relocation-model=pic -o data_transfer-hostpatched.s
 
-# verilator -sv -Wall -cc \
-#     ${SRC}/const.sv \
-#     ${SRC}/op_const.sv \
-#     ${SRC}/assert.sv \
-#     ${SRC}/core.sv \
-#     ${SRC}/global_mem_controller.sv \
-#     ${SRC}/gpu_controller.sv \
-#     ${SRC}/gpu_die.sv \
-#     ${SRC}/gpu_card.sv \
-#     --top-module gpu_card \
-#     --prefix gpu_card
+g++ -std=c++14 -fPIE -c data_transfer-hostpatched.s
+g++ -std=c++14 -fPIE -I${VERILATORDIR}/include -c ${VERILATORDIR}/include/verilated.cpp
 
-# (
-#     cd obj_dir
-#     make -f gpu_card.mk
-# )
-
-# g++ -std=c++11 -I${BASEDIR}/prot/verilator/prot_single_source -c ../sum_ints.cpp
-g++ -std=c++14 -c data_transfer-hostpatched.s
-g++ -std=c++14 -I${VERILATORDIR}/include -c ${VERILATORDIR}/include/verilated.cpp
-# g++ -std=c++14 -I obj_dir -I${VERILATORDIR}/include -c ${BASEDIR}/prot/verilator/prot_single_source/gpu_runtime.cpp
-
-# g++ -o sum_ints sum_ints-hostpatched.o gpu_runtime.o verilated.o obj_dir/controller__ALL.o
-g++ -o data_transfer data_transfer-hostpatched.o -L${BASEDIR}/prot/verilator/prot_single_source/build-cmake-mac -lverigpu_runtime
+g++ -o data_transfer data_transfer-hostpatched.o -L${GPURUNTIMEDIR} -lverigpu_runtime
 
 set +x
 
--- examples/cpp_single_source/sum_ints/run.sh
+++ examples/cpp_single_source/sum_ints/run.sh
@@ -16,17 +16,14 @@ SRC=${BASEDIR}/src
 export VERIGPUDIR=${BASEDIR}
 
 VERILATORDIR=/usr/local/share/verilator
-# CLANGDIR=$HOME/Downloads/clang+llvm-14.0.0-x86_64-apple-darwin
 
 if [[ $(uname) == Linux ]]; then {
     # assume installed clang 13 using https://apt.llvm.org/
     # if you want to handle other scenarios, please submit a PR :)
     echo Linux detected
-    # alias clang++=clang++-13
     CLANGPP=clang++-13
     CLANG=clang-13
     LLC=llc-13
-    # PATHHOSTSIDE=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-linux/patch_hostside
     GPURUNTIMEDIR=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-linux
     LIBEXPFS=-lstdc++fs
 } elif [[ $(uname) == Darwin ]]; then {
@@ -37,15 +34,14 @@ if [[ $(uname) == Linux ]]; then {
     LLC=${CLANGDIR}/bin/llc
     BUILDDIR=cmake-mac
     GPURUNTIMEDIR=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-mac
-    # PATHHOSTSIDE=${BASEDIR}/prot/verilator/prot_single_source/build-cmake-mac/patch_hostside
     MACCLTINCLUDE="-I/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk/usr/include"
 } fi
 
 cd ${SCRIPTDIR}
 
-# if [[ -d build_bash ]]; then {
-    # rm -R build_bash
-# } fi
+if [[ -d build_bash ]]; then {
+    rm -R build_bash
+} fi
 
 if [[ ! -d build_bash ]]; then {
     mkdir build_bash
@@ -53,16 +49,10 @@ if [[ ! -d build_bash ]]; then {
 
 cd build_bash
 
-# LIBRARY_PATH=${CLANGDIR}/
-
 # building patch_hostside is being migrated into cmake script at 
 # /prot/verilator/prot_single_source/CMakeLists.txt
 
 # host-side: -.cu => -hostraw.cll
-# clang++-13 --version
-# CLANGPP="clang++-13"
-# ${CLANGPP} --version
-# alias
 echo ${BASEDIR}
 ${CLANGPP} -fPIE \
     -std=c++11 -x cuda -nocudainc --cuda-host-only -emit-llvm \
@@ -98,36 +88,11 @@ echo patched hostside
 
 ${LLC} sum_ints-hostpatched.ll --relocation-model=pic -o sum_ints-hostpatched.s
 
-# verilator -sv -Wall -cc \
-#     ${SRC}/const.sv \
-#     ${SRC}/op_const.sv \
-#     ${SRC}/assert.sv \
-#     ${SRC}/core.sv \
-#     ${SRC}/global_mem_controller.sv \
-#     ${SRC}/gpu_controller.sv \
-#     ${SRC}/gpu_die.sv \
-#     ${SRC}/gpu_card.sv \
-#     --top-module gpu_card \
-#     --prefix gpu_card
-
-# (
-#     cd obj_dir
-#     make -f gpu_card.mk
-# )
-
-# g++ -std=c++11 -I${BASEDIR}/prot/verilator/prot_single_source -c ../sum_ints.cpp
 g++ -std=c++14 -fPIE -c sum_ints-hostpatched.s
 g++ -std=c++14 -fPIE -I${VERILATORDIR}/include -c ${VERILATORDIR}/include/verilated.cpp
-# g++ -std=c++14 -I obj_dir -I${VERILATORDIR}/include -c ${BASEDIR}/prot/verilator/prot_single_source/gpu_runtime.cpp
 
-# g++ -o sum_ints sum_ints-hostpatched.o gpu_runtime.o verilated.o obj_dir/controller__ALL.o
-# g++ -c ${BASEDIR}/prot/cpp/hostside/prot_upper_lower.cpp
 g++ -o sum_ints sum_ints-hostpatched.o -L${GPURUNTIMEDIR} -lverigpu_runtime ${LIBEXPFS}
-# g++ --pie -o sum_ints prot_upper_lower.o -L${GPURUNTIMEDIR} -lverigpu_runtime
-# ld --no-pie -o sum_ints sum_ints-hostpatched.o -L${GPURUNTIMEDIR} -lverigpu_runtime
 
 set +x
 
-# for i in {1..10}; do {
-    ./sum_ints +verilator+rand+reset+2 +verilator+seed+$(($RANDOM * 65536 + $RANDOM))
-# } done
+./sum_ints +verilator+rand+reset+2 +verilator+seed+$(($RANDOM * 65536 + $RANDOM))
