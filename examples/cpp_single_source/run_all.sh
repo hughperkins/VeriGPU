616072d551b0144ec45f3299edebe10ec17dde27
Hugh Perkins <hughperkins@gmail.com>
2022-04-16 13:20:15 -0400

    fix single source run_all

--- examples/cpp_single_source/run_all.sh
+++ examples/cpp_single_source/run_all.sh
@@ -4,7 +4,7 @@ set -ex
 set -o pipefail
 
 # first build the runtime and gpu...
-bash prot/verilator/prot_single_source/build-cmake.sh
+bash src/gpu_runtime/build-cmake.sh
 
 for ex in $(ls -b examples/cpp_single_source/*.cpp); do {
     bash examples/cpp_single_source/run.sh $(basename ${ex} .cpp)
