5f588c366e65750a1fb896b59a354a8b86c6ce54
Hugh Perkins <hughperkins@gmail.com>
2022-05-14 06:33:19 -0400

    axi4_mem_burst.sv prot

--- prot/misc/axi4_mem_burst.sv
+++ prot/misc/axi4_mem_burst.sv
@@ -83,11 +83,15 @@ module mem #(
     reg [2:0]              read_size;
     reg e_axi_burst        read_burst;
     reg [7:0]              read_len;
+    reg [7:0]              read_remaining;
+    // reg                    read_last;
 
     reg [ADDR_WIDTH - 1:0] write_addr;
+    reg [DATA_WIDTH - 1:0] read_data;
     reg [2:0]              write_size;
     reg e_axi_burst        write_burst;
     reg [7:0]              write_len;
+    reg [7:0]              write_remaining;
 
     reg [DATA_WIDTH - 1:0] mem[MEM_SIZE_BYTES >> 2];
 
@@ -103,32 +107,64 @@ module mem #(
             axi_read_state <= R_AWAIT_ADDR;
             axi_write_state <= W_AWAIT_ADDR;
         end else begin
-
             axi_rvalid <= 0;
             axi_arready <= 0;
-            axi_rlast <= 0;
+            // axi_rlast <= 0;
             case(axi_read_state)
                 R_AWAIT_ADDR: begin
                     axi_arready <= 1;
                     if(axi_arready & axi_arvalid) begin
                         $display("got read address %0d", axi_araddr);
                         read_addr <= axi_araddr;
+
+                        read_burst <= axi_arburst;
+                        read_size <= (1 << axi_arsize);
+                        read_len <= axi_arlen;
+                        read_remaining <= axi_arlen;
+                        $display("axi_arburst %0d", axi_arburst);
+                        assert(axi_arburst == AXI_BURST_INCR);
+                        $display("axi_arsize %0d", axi_arsize);
+                        $display("axi_arlen %0d", axi_arlen);
+                        assert((1 << axi_arsize) == 4);  // word at a time
+                        assert(axi_arlen == 4);      // 4 words
+
                         axi_read_state <= R_SENDING_DATA;
+                        $display("axi_arready <= 0");
                         axi_arready <= 0;
                         axi_rvalid <= 1;
+                        read_data <= mem[axi_araddr];
                         axi_rdata <= mem[axi_araddr];
+                        read_addr <= axi_araddr + (1 << axi_arsize);
+                        if(axi_arlen == 1) begin
+                            axi_rlast <= 1;
+                        end else begin
+                            axi_rlast <= 0;
+                        end
                     end
                 end
                 R_SENDING_DATA: begin
-                    // if(axi_rready) begin
-                    axi_rdata <= mem[read_addr];
+                    axi_rdata <= read_data;
                     axi_rvalid <= 1;
-                    axi_rlast <= 1;
                     if(axi_rready & axi_rvalid) begin
-                        // master confirmed receipt
-                        axi_rvalid <= 0;
-                        axi_read_state <= R_AWAIT_ADDR;
-                        axi_arready <= 1;
+                        $display(
+                            "valid ready read mem read_addr=%0d read_remaining=%0d",
+                            read_addr, read_remaining);
+                        if(read_remaining != 1) begin
+                            read_data <= mem[read_addr];
+                            axi_rdata <= mem[read_addr];
+                            read_addr <= read_addr + (1 << axi_arsize);
+                            read_remaining <= read_remaining - 1;
+                            if(read_remaining == 2) begin
+                                axi_rlast <= 1;
+                            end
+                        end
+                        if(read_remaining == 1) begin
+                            // axi_rlast <= 1;
+                            $display("mem finished burst read");
+                            axi_rvalid <= 0;
+                            axi_read_state <= R_AWAIT_ADDR;
+                            axi_arready <= 1;
+                        end
                     end
                     // end
                 end
@@ -143,11 +179,13 @@ module mem #(
                         $display("got write address %0d", axi_awaddr);
                         write_addr <= axi_awaddr;
                         write_burst <= axi_awburst;
-                        write_size <= axi_awsize;
+                        write_size <= (1 << axi_awsize);
                         write_len <= axi_awlen;
+                        write_remaining <= axi_awlen;
                         $display("axi_awburst %0d", axi_awburst);
                         assert(axi_awburst == AXI_BURST_INCR);
                         $display("axi_awsize %0d", axi_awsize);
+                        $display("axi_awlen %0d", axi_awlen);
                         assert((1 << axi_awsize) == 4);  // word at a time
                         assert(axi_awlen == 4);      // 4 words
 
@@ -161,10 +199,17 @@ module mem #(
                     if(axi_wready & axi_wvalid) begin
                         $display("write %0d to mem[%0d]", axi_wdata, write_addr);
                         mem[write_addr] <= axi_wdata;
-                        axi_write_state <= W_SENDING_RESPONSE;
-                        axi_wready <= 0;
-                        axi_bresp <= AXI_BRESP_OKAY;
-                        axi_bvalid <= 1;
+                        write_remaining <= write_remaining - 1;
+                        write_addr <= write_addr + write_size;
+
+                        if(write_remaining == 1) begin
+                            $display("got final write");
+                            assert(axi_wlast);
+                            axi_write_state <= W_SENDING_RESPONSE;
+                            axi_wready <= 0;
+                            axi_bresp <= AXI_BRESP_OKAY;
+                            axi_bvalid <= 1;
+                        end
                     end
                 end
                 W_SENDING_RESPONSE: begin
@@ -223,61 +268,98 @@ module dut #(
     input                         axi_bvalid,
     output reg                    axi_bready
 );
+    reg [7:0] write_remaining;
+    reg [7:0] read_remaining;
+
     task initiate_burst_write([ADDR_WIDTH - 1:0] addr, [7:0] len);
         axi_awaddr <= addr;
         axi_awburst <= AXI_BURST_INCR;
         axi_awsize <= $clog2(4);  // one word at a time
-        axi_awlen <= 4;
+        axi_awlen <= len;
         axi_awvalid <= 1;
-        assert(axi_awready);
+        write_remaining <= len;
+        assert(~axi_awvalid);
+        assert(~axi_awready | ~axi_awvalid);
+        assert(~axi_wready | ~axi_wvalid);
 
         #10
-        assert(~axi_awready);
+        assert(axi_awready & axi_awvalid);
+        assert(~axi_wready | ~axi_wvalid);
         axi_awvalid <= 0;
     endtask
 
     task write_mem([DATA_WIDTH - 1:0] data);
+        $display("write_mem %0d", data);
+        axi_wlast <= 0;
+        write_remaining <= write_remaining - 1;
         axi_wdata <= data;
         axi_wvalid <= 1;
-        assert(axi_wready);
+        if(write_remaining == 1) begin
+            $display("master wrote all");
+            axi_wlast <= 1;
+        end
 
-        #10
-        assert(~axi_wready);
-        axi_wvalid <= 0;
-        axi_bready <= 1;
-        assert(axi_bvalid);
-        
-        #10
-        axi_bready <= 0;
-        assert(~axi_bvalid);
+        #10;
+        assert(axi_wready & axi_wvalid);
+        if(write_remaining == 0) begin
+            axi_wvalid <= 0;
+
+            axi_bready <= 1;
+            
+            #10
+            assert(axi_bvalid & axi_bready);
+            assert(~axi_wready & ~axi_wvalid);
+            axi_bready <= 0;
+        end
     endtask
 
     task initiate_burst_read([ADDR_WIDTH - 1:0] addr, [7:0] len);
+        $display("initiate burst read");
         axi_araddr <= addr;
         axi_arburst <= AXI_BURST_INCR;
         axi_arsize <= $clog2(4);  // one word at a time
-        axi_arlen <= 4;
+        axi_arlen <= len;
         axi_arvalid <= 1;
-        assert(axi_arready);
+        read_remaining <= len;
+        assert(~axi_arvalid);
 
-        #10
-        assert(~axi_arready);
+        #10;
+        assert(axi_arready);
+        assert(axi_arvalid);
+
         axi_arvalid <= 0;
+    
+        axi_rready <= 1;
     endtask
 
     task read_mem([DATA_WIDTH - 1:0] expected_data);
-        axi_rready <= 1;
-        assert(axi_rvalid);
-        assert(axi_rdata == expected_data);
-        $display("read %0d from mem", axi_rdata);
+        #10;
+        assert(~axi_arready);
+        assert(~axi_arvalid);
 
-        #10
-        assert(~axi_rvalid);
-        axi_wvalid <= 0;
+        assert(axi_rvalid & axi_rready);
+        $display("read_mem expected=%0d read=%0d from mem", expected_data, axi_rdata);
+        assert(axi_rdata == expected_data);
+        read_remaining <= read_remaining - 1;
+        if(read_remaining == 1) begin
+            $display("master read last");
+            assert(axi_rlast);
+            axi_rready <= 0;
+        end
+
+        if(read_remaining == 1) begin
+            $display("master finished read");
+        end
     endtask
 
     initial begin
-        #30;
+        // $monitor(
+        //     "axi_awready=%0d axi_awvalid=%0d axi_wready=%0d axi_wvalid=%0d",
+        //     axi_awready, axi_awvalid, axi_wready, axi_wvalid);
+        // $monitor(
+        //     "axi_arready=%0d axi_arvalid=%0d axi_rready=%0d axi_rvalid=%0d",
+        //     axi_arready, axi_arvalid, axi_rready, axi_rvalid);
+        #40;
         $display("axi_aresetn %d", axi_aresetn);
 
         initiate_burst_write(20, 4);
@@ -306,9 +388,16 @@ module dut #(
     end
 
     always @(posedge axi_aclk) begin
+        
+    end
+
+    always @(posedge axi_aclk) begin
         if(~axi_aresetn) begin
-            $display("dut reset");
-        end else begin
+            axi_arvalid <= 0;
+            axi_rready <= 0;
+
+            axi_awvalid <= 0;
+            axi_wvalid <= 0;
         end
     end
 endmodule
@@ -420,21 +509,26 @@ module test_dut #(
 
     initial begin
         $display("clk initial");
+        axi_aclk = 1;
         forever begin
-            axi_aclk = 0;
             #5 axi_aclk = ~axi_aclk;
+            if(axi_aclk) begin
+                $display("");
+                $display("positive clock edge");
+            end
         end
     end
 
     initial begin
+        $dumpfile("test.vcd");
+        $dumpvars(0,test_dut);
+    end
+    
+    initial begin
         $display("reset initial");
         axi_aresetn <= 0;
         #20
-        // axi_aclk <= 0;
-        // #5 axi_aclk <= 1;
-        // #5 axi_aclk <= 0;
-        // #5 axi_aclk <= 1;
         axi_aresetn <= 1;
-        #200 $finish;
+        #280 $finish;
     end
 endmodule
